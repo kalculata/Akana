63ec03f30909b4c3a5bfa72b8aacb21de1d65f85
kalculata <nprincehuzaifa@gmail.com>
2021-07-24 10:08:36 +0200

    add main file

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    if(argc == 1)
+        cout << "il y a pas d'argument";
+    else
+        cout << "il y a d'argument";
+    return 0;
+}
