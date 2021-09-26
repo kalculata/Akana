34c7009423c79b725d701093644c52e2939cd674
kalculata <nprincehuzaifa@gmail.com>
2021-09-26 18:28:14 +0200

    release file config.php

--- example/config.php
+++ example/config.php
@@ -3,20 +3,15 @@
 
     define('ROOT', false);
     define('DEBUG', true);
-
     define('ROOT_CONTROLLER', ['file' => '../root.php', 'controller' => '\App\RootController']);
-
     define('APP_RESOURCES', [
         'users',
         'products',
-        'orders'
-    ]);
-
+        'orders']);
     define('DATABASE', [
         'type'      => DATABASE_TYPE,
         'host'      => DATABASE_HOST,
         'port'      => DATABASE_PORT,
         'name'      => DATABASE_NAME,
         'login'     => DATABASE_LOGIN,
-        'password'  => DATABASE_PASSWORD
-    ]);
+        'password'  => DATABASE_PASSWORD]);
